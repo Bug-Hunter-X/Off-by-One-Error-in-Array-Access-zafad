aef8d22b4cd96c2328b79346d6bdd76cae2aa2eb
BugHunter-X <askzenai@gmail.com>
2025-02-07 10:34:05 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,12 @@
+public class BugSolution {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2;
+        }
+        //To prevent error
+        if(arr.length > 0) {
+            System.out.println(arr[arr.length-1]);
+        }
+    }
+}
